64e67806416b36aa0cb47d5a003a05d3886984d3
hitruk andrey <xyz@hitruk.com>
2019-09-05 15:24:29 +0300

    final code

---
+++ tests/test_web.py
@@ -0,0 +1,13 @@
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+
+def test_basic_duckduckgo_search(browser):
+    PHRASE = 'panda'
+    search_page = DuckDuckGoSearchPage(browser)
+    search_page.load()
+    search_page.search(PHRASE)
+
+    result_page = DuckDuckGoResultPage(browser)
+    assert result_page.link_div_count() > 0
+    assert result_page.phrase_result_count(PHRASE) > 0
+    assert result_page.search_input_value() == PHRASE
